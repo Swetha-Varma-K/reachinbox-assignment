80bf51015b1d8c1339f056d2260761547dda5a33
Swetha Varma <swethakv05@gmail.com>
2026-09-05 21:12:04 +0530

    Make backend Render compatible

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -529,7 +529,7 @@ app.get("/auth/logout", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
+app.listen(Number(process.env.PORT) || 5000, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
